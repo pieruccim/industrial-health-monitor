1fd987a44a9a35476ada7f6da4140f698ef81088
pieruccim <matteo.pierucci4@gmail.com>
2023-01-29 12:40:43 -0500

    Minor fixes + first cooja simulation

--- coap-network/temperature/project-conf.h
+++ coap-network/temperature/project-conf.h
@@ -41,4 +41,6 @@
 
 #define LOG_LEVEL_APP LOG_LEVEL_DBG
 
+#define IEEE802154_CONF_PANID 0x0031
+
 #endif /* PROJECT_CONF_H_ */
--- coap-network/temperature/temperature_server.c
+++ coap-network/temperature/temperature_server.c
@@ -3,7 +3,10 @@
 #include <string.h>
 
 #include "project-conf.h"
+
 #include "contiki.h"
+#include "sys/etimer.h"
+#include "dev/leds.h"
 
 #include "routing/routing.h"
 
@@ -15,8 +18,10 @@
 #define LOG_LEVEL LOG_LEVEL_APP
 
 #define SERVER_EP "coap://[fd00::1]:5683"
+#define CONNECTION_TRY_INTERVAL 1
+#define REGISTRATION_TRY_INTERVAL 1
+
 #define SENSOR_NAME "temperature_sensor"
-#define REGISTRATION_EP "/registration"
 
 // variables to understand if the node is connected and registered
 static bool connected = false;
@@ -26,16 +31,19 @@ static bool registered = false;
 static struct etimer wait_connection;
 static struct etimer wait_registration;
 
+char *registration_ep = "/registration";
+
 static void check_router_reachable(){
     
     if (!NETSTACK_ROUTING.node_is_reachable()){
         
-        LOG_INFO("Border router is not reachable!");
+        LOG_INFO("Border router is not reachable!\n");
         etimer_reset( &wait_connection);
 
     } else{
         
-        LOG_INFO("Border router is reachable!");
+        LOG_INFO("Border router is reachable!\n");
+        leds_set(LEDS_NUM_TO_MASK(LEDS_BLUE));
         connected = true;
 
     }   
@@ -44,8 +52,8 @@ static void check_router_reachable(){
 void temp_chunck_handler(coap_message_t *response){
 
     if(response == NULL){
-        LOG_INFO("Registration request timed out! ");
-        etimer_set(&wait_connection, 1);
+        LOG_INFO("Registration request timed out! \n");
+        etimer_set(&wait_connection, CLOCK_SECOND * REGISTRATION_TRY_INTERVAL);
         return;
     }
 
@@ -54,9 +62,10 @@ void temp_chunck_handler(coap_message_t *response){
     printf("Registration response: %.*s", len, (char *) chunk);
 
     if( strcmp((char *) chunk, "Device registration completed!") == 0){
+        leds_set(LEDS_NUM_TO_MASK(LEDS_GREEN));
         registered = true;
     } else{
-        etimer_set(&wait_registration, 1);
+        etimer_set(&wait_registration, CLOCK_SECOND * REGISTRATION_TRY_INTERVAL);
     }
 
 }
@@ -71,7 +80,8 @@ PROCESS_THREAD(temperature_server, ev, data){
 
     PROCESS_BEGIN();
 
-    etimer_set( &wait_connection, 1);
+    leds_on(LEDS_NUM_TO_MASK(LEDS_RED));
+    etimer_set( &wait_connection, CLOCK_SECOND * CONNECTION_TRY_INTERVAL);
 
     while (!connected)
     {   
@@ -80,7 +90,8 @@ PROCESS_THREAD(temperature_server, ev, data){
         check_router_reachable();
     }
 
-    LOG_INFO("Temperature sensor is now CONNECTED through border router!");
+    
+    printf("Temperature sensor is now CONNECTED through border router!");
 
     while (!registered)
     {
@@ -90,7 +101,7 @@ PROCESS_THREAD(temperature_server, ev, data){
 
         //prepare message to send
         coap_init_message( request, COAP_TYPE_CON, COAP_POST, 0);
-        coap_set_header_uri_path(request, REGISTRATION_EP);
+        coap_set_header_uri_path(request, registration_ep);
         coap_set_payload(request, (uint8_t *)SENSOR_NAME, sizeof(SENSOR_NAME) - 1);
         
         // send blocking request, handle response on handler function
@@ -99,9 +110,9 @@ PROCESS_THREAD(temperature_server, ev, data){
         PROCESS_WAIT_UNTIL(etimer_expired(&wait_registration));
     }
 
-    LOG_INFO("Temperature sensor is now REGISTERED to registration server!");
+    LOG_INFO("Temperature sensor is now REGISTERED to registration server!\n");
 
-    LOG_INFO("Starting temperature server...");
+    LOG_INFO("Starting temperature server...\n");
 
     // activate the resource and 
 
